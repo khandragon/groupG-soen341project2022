6dbbe51174d2996422311042b8e54ba40dfd323e
Saad Khan <saaadkhan23@yahoo.ca>
2022-01-19 10:37:05 -0500

    create base react project

--- 341-frontend/src/App.js
+++ 341-frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Home from "./containers/Home.js";
+import About from "./containers/About.js";
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 
-function App() {
+const App = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "about", element: <About /> },
+  ]);
+  return routes;
+};
+
+const AppWrapper = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <App />
+    </Router>
   );
-}
+};
 
-export default App;
+export default AppWrapper;
---
+++ 341-frontend/src/containers/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import About from "./About";
+
+function Home(props) {
+  const navigate = useNavigate();
+
+  function onButtonClick() {
+    console.log("to about");
+    navigate("/about");
+  }
+
+  // function practice() {
+  //   let array = [];
+  //   ["saad", "yulia", "yason"].forEach((name) => {
+  //     console.log(name);
+  //     array.push(<About myuser={name} />);
+  //   });
+  //   return array;
+  // }
+
+  return (
+    <div>
+      <p>Hello Users</p>
+      {/* {practice()} */}
+      <Button onClick={onButtonClick}>Click</Button>
+    </div>
+  );
+}
+
+export default Home;
