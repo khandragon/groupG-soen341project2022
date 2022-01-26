3d3462f1637839bc1d18da6a13c7c815de0a1d23
Yason287 <78317885+Yason287@users.noreply.github.com>
2022-01-25 19:58:31 -0500

    refactored code with loop

--- 341-frontend/src/containers/Practice.js
+++ 341-frontend/src/containers/Practice.js
@@ -1,25 +1,27 @@
 import React from "react";
 import { Button, Card, Alert } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Practice() {
- 
-  
-    return (
-      <div>
-        <p>Testing Component</p>
-        <Alert variant="secondary" style={{ width: '15rem' }}>
-        
-  
-        <Button variant="info" style={{ width: '12rem' }}> <h6 align = "left">Name1 </h6></Button> <br /> <br />
-        <Button variant="info" style={{ width: '12rem' }}><h6 align = "left">Name2 </h6></Button><br /> <br />
-        <Button variant="info" style={{ width: '12rem' }}><h6 align = "left">Name3 </h6></Button><br /> <br />
-        <Button variant="info" style={{ width: '12rem' }}><h6 align = "left">Name4 </h6></Button><br /> <br />
-        <Button variant="info" style={{ width: '12rem' }}><h6 align = "left">Name5 </h6></Button>
-       
-        </Alert>
-      </div>
-    );
-  }
-  
-  export default Practice;
+  var arr = [1, 2, 3, 4, 5];
+  return (
+    <div>
+      <p>Testing Component</p>
+      <Alert variant="secondary" style={{ width: "15rem" }}>
+        {arr.map((val) => {
+          return (
+            <>
+              <Button variant="info" style={{ width: "12rem" }}>
+                {" "}
+                <h6 align="left">Name{val} </h6>
+              </Button>
+              <br />
+              <br />
+            </>
+          );
+        })}
+      </Alert>
+    </div>
+  );
+}
+export default Practice;
