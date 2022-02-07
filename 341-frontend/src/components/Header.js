82bfa9f35e340c493fe045ba1159279db3b15c66
khandragon <saaadkhan23@yahoo.ca>
2022-02-07 16:28:08 -0500

    implement header

--- 341-frontend/src/components/Header.js
+++ 341-frontend/src/components/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Nav, Navbar, NavDropdown,Container,Form, FormControl,Button } from "react-bootstrap";
+import { Nav, Navbar, Container, Image } from "react-bootstrap";
+import '../styles/components/Header.css';
+
 
 function Header(props) {
   const items = ["About", "Brands", "Products", "Sale", "Blog"];
@@ -9,28 +11,19 @@ function Header(props) {
   });
 
   return (
-<Navbar bg="light" expand="lg">
+<Navbar bg="light" expand="lg" className="main-header">
+<Image></Image>
+
   <Container fluid>
-    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
     <Navbar.Toggle aria-controls="navbarScroll" />
     <Navbar.Collapse id="navbarScroll">
       <Nav
-        className="me-auto my-2 my-lg-0"
+        className="m-auto my-2 my-lg-0"
         style={{ maxHeight: '100px' }}
         navbarScroll
       >
-        <Nav.Link href="#action1">Home</Nav.Link>
         {menuItems}
       </Nav>
-      <Form className="d-flex">
-        <FormControl
-          type="search"
-          placeholder="Search"
-          className="me-2"
-          aria-label="Search"
-        />
-        <Button variant="outline-success">Search</Button>
-      </Form>
     </Navbar.Collapse>
   </Container>
 </Navbar>
