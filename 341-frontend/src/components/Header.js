6acc370f9421705209c0266088b3217190193bc0
khandragon <saaadkhan23@yahoo.ca>
2022-02-08 12:50:03 -0500

    import icons, implement and make clickable links

--- 341-frontend/src/components/Header.js
+++ 341-frontend/src/components/Header.js
@@ -1,31 +1,54 @@
 import React from "react";
-import { Nav, Navbar, Container, Image } from "react-bootstrap";
-import '../styles/components/Header.css';
-
+import { Nav, Col, Row, Image } from "react-bootstrap";
+import "../styles/components/Header.css";
+import logo from "../images/image1.png";
+import { BsFillPersonFill, BsFillCartFill } from "react-icons/bs";
 
 function Header(props) {
-  const items = ["About", "Brands", "Products", "Sale", "Blog"];
+  const items = [
+    "About",
+    "Brands",
+    "Products",
+    "Sale",
+    "Blog",
+    "Profile",
+    "Cart",
+  ];
+
   let menuItems = [];
   items.forEach((item) => {
-    menuItems.push(<Nav.Link href={item}>{item}</Nav.Link>);
+    if (item === "Profile") {
+      menuItems.push(
+        <div className="iconBtn">
+          <a href={item}>
+            <BsFillPersonFill size={30} />
+          </a>
+        </div>
+      );
+    } else if (item === "Cart") {
+      menuItems.push(
+        <div className="iconBtn">
+          <a href={item}>
+            <BsFillCartFill size={30} />
+          </a>
+        </div>
+      );
+    } else {
+      menuItems.push(<Nav.Link href={item}>{item}</Nav.Link>);
+    }
   });
 
   return (
-<Navbar bg="light" expand="lg" className="main-header">
-<Image></Image>
-
-  <Container fluid>
-    <Navbar.Toggle aria-controls="navbarScroll" />
-    <Navbar.Collapse id="navbarScroll">
-      <Nav
-        className="m-auto my-2 my-lg-0"
-        style={{ maxHeight: '100px' }}
-        navbarScroll
-      >
-        {menuItems}
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-  )}
+    <div className="main-header">
+      <Image src={logo} className="header-item"></Image>
+      <Row className="d-flex justify-content-center">
+        <Col xs lg="1"></Col>
+        <Col lg="8" className="header-list">
+          <Nav justify>{menuItems}</Nav>
+        </Col>
+        <Col xs lg="1"></Col>
+      </Row>
+    </div>
+  );
+}
 export default Header;
