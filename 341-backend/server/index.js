83e5f66e5faa18a5a47e3c5624a39f7f04247e4d
Saad Khan <saaadkhan23@yahoo.ca>
2022-02-21 02:48:56 -0500

    add data

--- 341-backend/server/index.js
+++ 341-backend/server/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
-
-const PORT = process.env.PORT || 3001;
-
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 27017;
 const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
