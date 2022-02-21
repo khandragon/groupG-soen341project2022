73f11ea7e7425d9892a9b33a16b2a6d9aba501cf
Saad Khan <saaadkhan23@yahoo.ca>
2022-02-21 02:00:54 -0500

    remove unused

--- 341-frontend/src/components/Buttons/AddCartButton.js
+++ 341-frontend/src/components/Buttons/AddCartButton.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
 
 function AddCartButton(props) {
   function onButtonClick() {
     console.log("To cart with ID: " + props.itemID);
-    //navigate("/cart");
   }
+
   return (
     <div class="col-md-12 text-center">
       <Button onClick={onButtonClick} color="orange" size="lg">
--- 341-frontend/src/containers/Home.js
+++ 341-frontend/src/containers/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import About from "./About";
 
 function Home(props) {
   const navigate = useNavigate();
