aa7c2e13e01588e03b3fe30d1ffe64d6bc70dc6b
Saad Khan <saaadkhan23@yahoo.ca>
2022-03-17 13:00:03 -0400

    add and style about text

--- 341-frontend/src/containers/About.js
+++ 341-frontend/src/containers/About.js
@@ -3,12 +3,28 @@ import "../styles/About.css";
 
 function About(props) {
   return (
-    <div>
-      <h1>About</h1>
-      <p className="text-heading">
-        Welcome to Locazie a site made for local produce5rs to sell their goods
-        to others world wide
-      </p>
+    <div className="about-text">
+      <h1>
+        <u>About Us</u>
+      </h1>
+      <br />
+      <h1>
+        Created in March 2022, LOCALIZE has for a mission to support local
+        economy by allowing all small businessses as well as artizans to sell
+        their products within our community.
+      </h1>
+      <br />
+
+      <h1>
+        It is a one stop shop that carries a variety of high quality products,
+        hand made or locally sourced products.
+      </h1>
+      <br />
+
+      <h1>
+        From books to souvenirs to food - you can find all of it in our e-store!
+        From all our team and our businesses - Thank you for shopping local!
+      </h1>
     </div>
   );
 }
