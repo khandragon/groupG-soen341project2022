daa91ec553cce04f185c8d1aa2c499ed131c0d81
Saad Khan <saaadkhan23@yahoo.ca>
2022-03-20 16:39:31 -0400

    fix index and changes

--- 341-frontend/src/containers/Home.js
+++ 341-frontend/src/containers/Home.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
-import Inventory from "./Inventory";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import SearchBarProducts from "../components/Buttons/SearchBarProducts";
 import ProductCard from "../components/ProductCard";
 import { useNavigate } from "react-router-dom";
 import "../styles/components/Home.css";
@@ -26,15 +23,12 @@ function Home(props) {
       updatedAt: "",
     },
   ]);
-  var test = {};
   useEffect(() => {
     getAllProducts().then((res) => {
-      setInventory(res);
+      setInventory(res.slice(0, 4));
       console.log(res);
     });
   }, []);
-
-  const inventoryItems = [test, test, test, test];
 
   function onButtonClick() {
     navigate("/about");
@@ -42,9 +36,6 @@ function Home(props) {
 
   return (
     <div>
-      {/* <p>Hello Users</p>
-      <Button onClick={onButtonClick}>Click</Button> */}
-
       <h3 className="rectangle2">
         Thank You for Shopping Local{" "}
         <img
@@ -59,7 +50,7 @@ function Home(props) {
       <h2 className="OurFam">NEW IN OUR FAMILY</h2>
       <br></br>
       <Row xs={1} md={2} className="g-4 card-holder">
-        {inventoryItems.map((value) => {
+        {inventory.map((value) => {
           return (
             <Col>
               <ProductCard
