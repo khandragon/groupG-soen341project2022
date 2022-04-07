b67c644200701363c16f820cbced6004b00d5052
cindykhin <cicispm@gmail.com>
2022-04-06 20:56:33 -0400

    update

--- 341-backend/routes/products-router.js
+++ 341-backend/routes/products-router.js
@@ -10,15 +10,10 @@ router.get("/products/:isbn", productsCtrl.getProductInformation);
 router.put("/products/:isbn", productsCtrl.updateProductInformation);
 router.post("/products/", productsCtrl.createProductInformation);
 router.delete("/products/:isbn", productsCtrl.deleteProduct);
-<<<<<<< HEAD
-router.get("/productsCategory/:category", productsCategoryCtrl.getProductsCategory);
-router.get("products/", homepageCtrl.getHomeProducts);
-=======
 router.get(
   "/productsCategory/:category",
   productsCategoryCtrl.getProductsCategory
 );
 router.get("/productsCategory/", productsCategoryCtrl.getAllProductsCategory);
->>>>>>> c56ad34c4e829e43b9423770898afb6a3342cc83
 
 module.exports = router;
