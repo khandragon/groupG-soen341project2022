adf24609dc22eb3971aa2c9a9d56ae0c44b794bf
cindykhin <cicispm@gmail.com>
2022-04-07 20:05:14 -0400

    HomeProducts function, router, api

--- 341-backend/controller/homepage-ctrl.js
+++ 341-backend/controller/homepage-ctrl.js
@@ -1,22 +1,24 @@
-
 const Products = require("../schemas/products-model");
 const { deleteBusinessLink } = require("./businessProducts-ctrl");
 
-getHomeProducts = async (req, res) => {
-    var randomNumber = Math.floor(Math.random() * (products.length() - 1));
-    var posted = new Array();
+// get products in random order to appear on homepage
+getHomeProducts = async (req, res) => {  
     try {
-        while (posted.length() <= products.length()) {
-            if (posted.includes(randomNumber)) {
-                randomNumber = Math.floor(Math.random() * (products.length() - 1));
-            }
-            else {
-            const products = await Products.find( { $arrayElemAt: "products", randomNumber });
-            posted.push(randomNumber);
-            }
+        const homeAmount = req.params.homeAmount;
+        const homepage = await Products.find({}); // list of products from database
+        var homeProducts = new Array();
+        function randomNum() { // function gets random index from list of products
+            var rnb = Math.floor(Math.random() * homepage.length); 
+            return rnb;
         };
-        
-        return res.status(200).json({ success: true, data: products });
+		var index = randomNum();
+        for (var i = 0; i < homeAmount; i++) {
+			while (homeProducts.includes(homepage[index])) { // while loop to get unique product
+                index = randomNum(); // if product is already listed for homepage, find another product
+            };
+            homeProducts[i] = homepage[index]; // add product to homepage
+        }
+        return res.status(200).json({ success: true, data: homeProducts });
     } catch (e) {
         console.log(e);
         return res
