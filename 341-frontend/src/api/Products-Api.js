d5e82bcd7e704b0130615307e29d1df34368ff54
Eggsaminator <bourouf1@hotmail.fr>
2022-04-10 15:19:28 -0400

    conflictless update for final sprint

--- 341-frontend/src/api/Products-Api.js
+++ 341-frontend/src/api/Products-Api.js
@@ -1,18 +1,27 @@
-import { addBusinessProduct } from "./BusinessProducts-Api";
-
 const axios = require("axios");
 const api = "http://localhost:8000/api/products/";
-
+/**
+ * get all the products in the database
+ * @returns a list of all the products
+ */
 async function getAllProducts() {
   const response = await axios.get(api);
   return response.data.data;
 }
-
+/**
+ * get a specific product 
+ * @param {*} isbn isbn of the product to get 
+ * @returns body of the product found, if any
+ */
 async function getProductByIsbn(isbn) {
   const response = await axios.get(api + isbn);
   return response.data.data;
 }
-
+/**
+ * get multipple products 
+ * @param {*} isbnList list of the isbns of the products to get
+ * @returns list of the products
+ */
 async function getMultipleProductsByIsbn(isbnList) {
   let products = [];
 
@@ -22,7 +31,11 @@ async function getMultipleProductsByIsbn(isbnList) {
   }
   return products;
 }
-
+/**
+ * update the information of a product
+ * @param {*} isbn isbn of the product to update
+ * @param {*} product the new product information
+ */
 async function updateProductByIsbn(isbn, product) {
   try {
     await axios.put(api + isbn, product).then(function (result) {
@@ -32,7 +45,12 @@ async function updateProductByIsbn(isbn, product) {
     console.error(error);
   }
 }
-
+/**
+ * creates a new product and adds it to the database
+ * @param {*} product the product to add
+ * @param {*} username the product seller
+ * @returns the success status of this method
+ */
 async function createNewProduct(product, username) {
   await axios.post(api, product).then((result) => {
     const data = {
